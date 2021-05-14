c321e456decadc7ca0b4020f5e01ddb57688a9a5
anantaihza <anantaihza21@gmail.com>
2021-05-14 08:52:16 +0700

    update navbar admin

--- src/components/NavbarAdmin.vue
+++ src/components/NavbarAdmin.vue
@@ -1,34 +1,5 @@
 <template>
   <div>
-    <!-- <nav class="navbar navbar-light fixed-top">
-      
-      <span class="navbar-brand mb-0 h4 ml-auto">
-        Jhon Doe
-      </span>
-      <button
-        class="navbar-toggler ml-auto"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNavAltMarkup"
-        aria-controls="navbarNavAltMarkup"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <span class="navbar-brand mb-0 h4 text-center d-block">
-          Jhon Doe
-        </span>
-        <div class="navbar-nav text-center">
-          <a class="nav-item nav-link" href="#">Dashboard</a>
-          <a class="nav-item nav-link" href="#">Kandidat</a>
-          <a class="nav-item nav-link" href="#">Pemilih</a>
-          <a class="nav-item nav-link" href="#">Sign Out</a>
-        </div>
-      </div>
-    </nav> -->
 
     <nav class="navbar navbar-expand-lg navbar-light">
       <a class="navbar-brand d-block d-sm-block d-md-none" href="#">Volma</a>
@@ -100,11 +71,20 @@ export default {
 }
 
 @media only screen and (max-width: 768px) {
+  /* .navbar-toggler {
+    display: inline-block;
+  } */
+  /* .navbar .navbar-brand {
+    display: none;
+  } */
+}
+
+@media only screen and (max-width: 990px) {
   .navbar-toggler {
     display: inline-block;
   }
-  .navbar .navbar-brand {
+  /* .navbar .navbar-brand {
     display: none;
-  }
+  } */
 }
 </style>
