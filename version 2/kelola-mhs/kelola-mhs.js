f5b5aa91e7dd66c71922bbe42110d5b6c28bdd07
salmanFarhan <48727962+FarhanLhant@users.noreply.github.com>
2021-06-05 00:03:58 +0700

    Delete version 2 directory

--- version 2/kelola-mhs/kelola-mhs.js
+++
@@ -1,7 +0,0 @@
-/*===== LINK ACTIVE  =====*/ 
-const linkColor = document.querySelectorAll('.navigate-link')
-function colorLink(){
-  linkColor.forEach(l=> l.classList.remove('active'))
-  this.classList.add('active')
-}
-linkColor.forEach(l=> l.addEventListener('click', colorLink))
